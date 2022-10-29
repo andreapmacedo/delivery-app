ee98c9bab399e5e1f0020b45eab2eae8b7ae4a7a
andreapmacedo <aa.andremacedo@gmail.com>
2022-10-29 13:35:27 -0300

    feat: alteração da url.

---
+++ front-end/src/pages/client/orders-details/ClientOrdersDetails.jsx
@@ -0,0 +1,15 @@
+// import React, { useContext } from 'react';
+// import { useNavigate, useLocation } from 'react-router-dom';
+// import MainContext from '../../Context/MainContext';
+// import { ShoppingCart, MagnifyingGlass } from 'phosphor-react';
+// import './style.css';
+
+export default function ClientOrdersDetails() {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+ 
---
+++ front-end/src/pages/client/orders/ClientOrders.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+// import { useNavigate, useLocation } from 'react-router-dom';
+// import MainContext from '../../Context/MainContext';
+// import { ShoppingCart, MagnifyingGlass } from 'phosphor-react';
+import './style.css';
+
+export default function ClientOrders() {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
