228c2e1e2e1c2e7b2002ec82b62b17ffe750542e
rabeloguedes <rabeloguedes@proton.me>
2022-10-29 15:16:15 -0300

    refactory: Altera forma de armazenar os dados no estado.

--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -1,24 +1,24 @@
 import { useState } from 'react';
+import { useNavigation } from 'react-router-dom';
 import login from '../../services/APIs';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigation();
 
   const toLogin = async (e) => {
     e.preventDefault();
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
-    setEmail(emailInput.value);
-    setPassword(passwordInput.value);
-    console.log(email, 'email');
-    console.log(password, 'senha');
     try {
       const user = await login(email, password);
       console.log(user);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const goToRegistration = () => {
+    navigate('/registre');
   };
 
   return (
@@ -30,6 +30,9 @@ export default function Login() {
             type="email"
             placeholder="your@email.com"
             autoComplete="off"
+            onChange={ ({ target }) => setEmail(target.value) }
+            value={ email }
+            data-testid="common_login__input-email"
           />
         </label>
         <label htmlFor="password">
@@ -38,9 +41,24 @@ export default function Login() {
             type="password"
             placeholder="******"
             autoComplete="off"
+            onChange={ ({ target }) => setPassword(target.value) }
+            value={ password }
+            data-testid="common_login__input-password"
           />
         </label>
-        <button type="submit">Entrar</button>
+        <button
+          type="submit"
+          data-testid="common_login__button-login"
+        >
+          Entrar
+        </button>
+        <button
+          type="button"
+          data-testid="common_login__button-login"
+          onClick={ goToRegistration }
+        >
+          Ainda não tenho conta
+        </button>
       </form>
     </section>
   );
