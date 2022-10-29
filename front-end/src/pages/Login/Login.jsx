a6bcafc41e595f1742de7fca171feeba5b70f990
rabeloguedes <rabeloguedes@proton.me>
2022-10-29 13:01:53 -0300

    feat: Modifca o FrontEnd para fazer a requisição do login.

--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
+import login from '../../services/APIs';
+
 export default function Login() {
-  const login = (e) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const toLogin = async (e) => {
     e.preventDefault();
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    setEmail(emailInput.value);
+    setPassword(passwordInput.value);
+    console.log(email, 'email');
+    console.log(password, 'senha');
+    try {
+      const user = await login(email, password);
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <section>
-      <form onSubmit={ (e) => login(e) }>
+      <form onSubmit={ (e) => toLogin(e) }>
         <label htmlFor="email">
           <input
             id="email"
