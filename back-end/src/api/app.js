a65fadd258fc8798f09a7337ab3ba2db61d330b2
andreapmacedo <aa.andremacedo@gmail.com>
2022-11-01 18:52:25 -0300

    feat: serve as imagens para o front.

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static('public'));
 app.use('/login', routeLogin);
 app.use('/register', routeRegister);
 app.use('/customer/products', routeProducts);
