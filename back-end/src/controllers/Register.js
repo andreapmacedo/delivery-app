4a6f1d7c4913397c83bb722ddccd978b891d5115
rabeloguedes <rabeloguedes@proton.me>
2022-11-01 15:25:18 -0300

    refactory: Melhora o código do service e controller do register.

--- back-end/src/controllers/Register.js
+++ back-end/src/controllers/Register.js
@@ -1,25 +1,15 @@
-const { verifyUserRegister, registerService } = require('../services/Register');
+const { registerService } = require('../services/Register');
 const { generateToken } = require('../middlewares/auth');
-
-const verifyRegister = async (name, email) => {
-  const verify = await verifyUserRegister(name, email);
-
-  return verify;
-};
 
 const registerController = async (req, res) => {
   const { name, email, password } = req.body;
-  const verifyNameEmail = await verifyRegister(name, email);
-
-  if (verifyNameEmail?.name === name) {
-    return res.status(409).json({ message: 'Name already registered' });
-  }
-
-  if (verifyNameEmail?.email === email) {
-    return res.status(409).json({ message: 'Email already registered' });
-  }
-  
   const newUser = await registerService({ name, email, password, role: 'customer' });
+  const { status, message } = newUser;
+
+  if (status && message) {
+    return res.status(status).json({ message });
+  }
+
   const token = generateToken({ email, role: newUser.role });
   return res.status(201).json(token);
 };
