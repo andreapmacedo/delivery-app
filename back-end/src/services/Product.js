8fb83ef0509e769a4519c14cfa7ea99d7e5e922c
Amanda Zambelli <amandazambelli@ymail.com>
2022-11-01 16:58:28 -0300

    fix: arruma linter

--- back-end/src/services/Product.js
+++ back-end/src/services/Product.js
@@ -1,6 +1,6 @@
 const { Product } = require('../database/models');
 
-const productsService = async (email, password) => {
+const productsService = async () => {
   const getAll = await Product.findAll();
 
   return getAll;
