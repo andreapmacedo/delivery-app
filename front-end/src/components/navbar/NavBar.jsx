7e689792f477d0b5f84670a23daa9f6500eb5730
andreapmacedo <aa.andremacedo@gmail.com>
2022-11-01 17:36:03 -0300

    refactory: adiciona links aos botões da navbar.

--- front-end/src/components/navbar/NavBar.jsx
+++ front-end/src/components/navbar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import MainContext from '../../context/MainContext';
 
 export default function NavBar() {
@@ -6,19 +7,39 @@ export default function NavBar() {
     user,
   } = useContext(MainContext);
   return (
-    <div>
-      <div className="left">
-        <div className="button">PRODUTOS</div>
-        <div className="button">MEUS PEDIDOS</div>
-      </div>
-      <div className="right">
-        <h1>{ user }</h1>
+    <nav>
+      <div className="nav-left">
         <div
-          className="button"
+          className="nav-button"
+          datatestid="customer_products__element-navbar-link-products"
         >
-          sair
+          <Link to="/customer/products">
+            PRODUTOS
+          </Link>
+        </div>
+        <div
+          className="nav-button"
+          datatestid="customer_products__element-navbar-link-orders"
+        >
+          <Link to="/customer/products">
+            MEUS PEDIDOS
+          </Link>
         </div>
       </div>
-    </div>
+      <div
+        className="nav-right"
+        datatestid="customer_products__element-navbar-user-full-name"
+      >
+        <h1>{ user }</h1>
+        <button
+          type="button"
+          className="nav-button"
+          datatestid="customer_products__element-navbar-link-logout"
+          // onClick={ () => localStorage.clear() }
+        >
+          SAIR
+        </button>
+      </div>
+    </nav>
   );
 }
