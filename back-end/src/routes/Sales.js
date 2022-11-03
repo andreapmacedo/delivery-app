86d3be7254c22e1273aef9c5c1ef3e5ca3288886
Leonardo Silva <leonardorc.geo@gmail.com>
2022-11-03 20:33:07 -0300

    feat: Criação da rota /sales:id

--- back-end/src/routes/Sales.js
+++ back-end/src/routes/Sales.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const SalesController = require('../controllers/Sales');
 const verifyOrder = require('../middlewares/orderVerification');
+const verifyToken = require('../middlewares/tokenValidation');
 
 const route = Router();
 
-route.post('/', verifyOrder, SalesController.create);
+route.post('/', verifyToken, verifyOrder, SalesController.create);
 route.get('/', SalesController.getAll);
+route.get('/:id', SalesController.getById);
+
 
 module.exports = route;
