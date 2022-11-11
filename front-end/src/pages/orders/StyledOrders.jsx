9ab9461541d688f299c68e7e8945e7dc82a611d7
andreapmacedo <aa.andremacedo@gmail.com>
2022-11-11 20:59:33 -0300

    feat: implementa estilo para card de pedidos.

---
+++ front-end/src/pages/orders/StyledOrders.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledOrders = styled.div`
+  padding: 16px 16px;
+  gap: 16px;
+  display: flex;
+  background-color: var(--light-text);
+  flex-flow: row wrap;
+  min-height: 93vh;
+`;
+
+export default StyledOrders;
